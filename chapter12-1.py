3d9a9b1d25133bb76b98c3ee8f0103051ed2000b
jupiter <m17600516549@163.com>
2017-07-11 18:12:28 +0800

    chapter 12-2

--- chapter12-1.py
+++ chapter12-1.py
@@ -3,5 +3,6 @@
 names = str(raw_input("Enter 5 name:"))
 name = ['z', 'x', 'c', 'v', 'b']
 for name in names:
-    print name 
-print "The names are", names
+    print names
+
+
---
+++ chapter12-2.py
@@ -0,0 +1,29 @@
+dictionary = {}
+flag = 'a'
+pape = 'a'
+off = 'a'
+while flag == 'a' or 'c':
+    flag = raw_input("Add or look up a word(a/1)?")
+    if flag == "a":
+        word = raw_input("Type the word:")
+        defintion = raw_input("Type the defintion:")
+        dictionary[str(word)] = str(defintion)
+         print"Word added!"
+         pape = raw_input("Add or look up a word (a/1)?")
+         if pape == 'a':
+             print dictionary
+         else:
+             continue
+    elif flag == 'c':
+        check_word = raw_input("type the word:")
+         for key in sorted(dictionary.keys()):
+             if str(check_word) == key:
+                 print "existence", key, dictionary[key]
+                 break
+             else:
+                 off = 'b'
+         if off == 'b':
+             print "That word isn't in the dictionary yet."
+         else:
+             print "error type"
+             break
