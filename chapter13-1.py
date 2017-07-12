384ae8dce84844cf0aadd94aacb12b2aa7c1d3b4
jupiter <m17600516549@163.com>
2017-07-12 21:49:02 +0800

    chapter13-1-2

---
+++ chapter13-1.py
@@ -0,0 +1,9 @@
+def graph(n):
+    if n == 1:
+        print '   cccc          a            rrrrr         ttttttt         eeeeee'
+        print '  c    c        a  a          r    r           t            e'
+        print ' c             a    a         r    r           t            eeee'
+        print ' c            aaaaaaaa        rrrrr            t            e'
+        print '  c    c     a        a       r    r           t            e'
+        print '   cccc     a          a      r     r          t            eeeeee'
+graph(1)
---
+++ chapter13-2.py
@@ -0,0 +1,7 @@
+def len(str):
+    x = 0
+    for i in str:
+        x += 1
+    print u'The length of string is:' ,x
+    return x
+len('asdfghjklpoiuytrewqzxcvbnm')
