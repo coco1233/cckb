cb5462028f19d6f541c286fd3eee2f403823bf6d
jupiter <m17600516549@163.com>
2017-07-21 15:25:26 +0800

    第21章第一题

---
+++ chapter21-1.py
@@ -0,0 +1,7 @@
+name = str(raw_input("What is your name?:"))
+age = str(raw_input("How old are you?:"))
+color = str(raw_input("What is your favourite color?:"))
+
+print "Your name is", name,
+print "you are ", age, "years old, ", 
+print "and you like the color", color
